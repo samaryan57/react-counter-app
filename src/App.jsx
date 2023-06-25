3fa129387fe226cede3e42a224f925db0ce4cf58
samaryan57 <samanta2@purdue.edu>
2023-06-25 19:27:52 +0530

    Use native redux dependency to create a counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 const App = () => {
+
+    const counter = useSelector(state => state.counter);
+    const dispatch = useDispatch();
+    
+    const increment = () => {
+        dispatch({type: "INC"});
+    };
+
+    const decrement = () => {
+        dispatch({type: "DEC"});
+    };
+
+    const addByTen = () => {
+        dispatch({type: "ADD", payload: 10});
+    };
+
     return (
-        <h1>Hello World!</h1>
+        <div>
+            <h1>Counter App</h1>
+            <h2>{counter}</h2>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button> 
+            <button onClick={addByTen}>Add By 10</button>
+        </div>
     );
 };
 
